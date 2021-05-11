d44e67e58a5d6eea67889fa3c632394b02c9cecd
Jair Novello <jaja505@hotmail.com>
2021-05-11 13:38:32 -0400

    Create Framework

--- src/test/java/com/nextbaseCRM/tests/test.java
+++ src/test/java/com/nextbaseCRM/tests/test.java
@@ -5,12 +5,4 @@ import com.nextbaseCRM.utilities.ConfigurationReader;
 import com.nextbaseCRM.utilities.Driver;
 
 public class test {
-    public static void main(String[] args) {
-
-       Driver.getDriver().get(ConfigurationReader.getProperty("url"));
-
-        System.out.println(ConfigurationReader.getProperty("url"));
-
-
-    }
 }
--- src/test/java/com/nextbaseCRM/utilities/BrowserUtils.java
+++ src/test/java/com/nextbaseCRM/utilities/BrowserUtils.java
@@ -1,4 +1,43 @@
 package com.nextbaseCRM.utilities;
 
+import org.junit.Assert;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
 public class BrowserUtils {
+
+    public static void titleVerification(String expectedTitle){
+
+        String actualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+    }
+
+    public static void hoverOver(WebElement element){
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(element).perform();
+    }
+
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
+            System.out.println("something happened in sleep method");
+
+        }
+    }
+
+    public static WebElement waitForVisibility(WebElement element, int timeInSecond){
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),timeInSecond);
+
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+//
 }

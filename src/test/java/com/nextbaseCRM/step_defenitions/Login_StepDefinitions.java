4d553086cbf07e57055b9a8e523c7855072366e9
Akita <jamila.osmn@gmail.com>
2021-05-12 08:05:02 -0700

    step_defination deleted

--- src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
+++ src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
@@ -1,10 +1,13 @@
 package com.nextbaseCRM.step_defenitions;
 
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
 public class Login_StepDefinitions {
     @When("user enters hr username")
     public void user_enters_hr_username() {
         // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+
     }
 
 
@@ -27,34 +30,11 @@ public class Login_StepDefinitions {
     }
 
 
-
-    @When("user enters password")
-    public void user_enters_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user should see homepage")
-    public void user_should_see_homepage() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-
     @When("user enters helpDesk username")
     public void user_enters_help_desk_username() {
         // Write code here that turns the phrase above into concrete actions
         throw new io.cucumber.java.PendingException();
     }
 
-
-    @When("user enters password")
-    public void user_enters_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
-    @Then("user should see homepage")
-    public void user_should_see_homepage() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
-    }
 
 }

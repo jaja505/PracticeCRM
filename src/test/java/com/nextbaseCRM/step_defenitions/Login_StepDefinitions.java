04e51965895e29117e6d41b8f974d8fca9ff2981
Jair Novello <jaja505@hotmail.com>
2021-05-12 10:53:21 -0400

    Add login.feature

--- src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
+++ src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
@@ -1,6 +1,60 @@
 package com.nextbaseCRM.step_defenitions;
 
 public class Login_StepDefinitions {
+    @When("user enters hr username")
+    public void user_enters_hr_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
+
+    @When("user enters password")
+    public void user_enters_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see homepage")
+    public void user_should_see_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("user enters marketing username")
+    public void user_enters_marketing_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+
+    @When("user enters password")
+    public void user_enters_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see homepage")
+    public void user_should_see_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters helpDesk username")
+    public void user_enters_help_desk_username() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
+    @When("user enters password")
+    public void user_enters_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("user should see homepage")
+    public void user_should_see_homepage() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 
 }

1bab4918acf06c33ad4ecc01a3f8a943da5b01f5
Jair Novello <jaja505@hotmail.com>
2021-05-12 11:35:30 -0400

    LoginPage.java

--- src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
+++ src/test/java/com/nextbaseCRM/step_defenitions/Login_StepDefinitions.java
@@ -1,21 +1,25 @@
 package com.nextbaseCRM.step_defenitions;
 
+import com.nextbaseCRM.pages.LoginPage;
+import com.nextbaseCRM.utilities.ConfigurationReader;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-public class Login_StepDefinitions {
+public class Login_StepDefinitions extends LoginPage {
+
+    LoginPage loginPage = new LoginPage();
+
     @When("user enters hr username")
     public void user_enters_hr_username() {
-        // Write code here that turns the phrase above into concrete actions
-
+        loginPage.inputUsername.sendKeys(ConfigurationReader.getProperty("hr1"));
     }
 
 
     @When("user enters password")
     public void user_enters_password() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new io.cucumber.java.PendingException();
+loginPage.inputPassword.sendKeys(ConfigurationReader.getProperty("password"));
     }
+
     @Then("user should see homepage")
     public void user_should_see_homepage() {
         // Write code here that turns the phrase above into concrete actions
